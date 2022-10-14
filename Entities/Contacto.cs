6eea7f0a937c5db1feb6da7096e39a520267ea36
Marcos Diaz Bastida <mdiazbastida@gmail.com>
2022-10-13 22:44:22 -0400

    Creacion del las Entidades, el Contexto de la BD y creacion de la cadena de conexion

---
+++ Entities/Contacto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContactoApi.Entities
+{
+    public partial class Contacto
+    {
+        public int ContactoId { get; set; }
+        public string Contacto1 { get; set; }
+        public int? Telefono { get; set; }
+        public string Correo { get; set; }
+        public string Direccion { get; set; }
+    }
+}
---
+++ Entities/ContactosDBContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ContactoApi.Entities
+{
+    public partial class ContactosDBContext : DbContext
+    {
+        public ContactosDBContext()
+        {
+        }
+
+        public ContactosDBContext(DbContextOptions<ContactosDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Contacto> Contacto { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-2RELMQS;Initial Catalog=ContactosDB;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=False");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Contacto>(entity =>
+            {
+                entity.Property(e => e.ContactoId).HasColumnName("ContactoID");
+
+                entity.Property(e => e.Contacto1)
+                    .IsRequired()
+                    .HasColumnName("Contacto")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Correo)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Direccion)
+                    .IsRequired()
+                    .HasMaxLength(500)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
